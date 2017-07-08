19e34ac76387f0667cdd646e7a3d8feee457aae8
minsoo.jun <minsoo.jun@rakuten.com>
2017-07-09 08:40:33 +0900

    5.2.2 대

--- mvc/src/main/java/sample/customer/config/BizConfig.java
+++ mvc/src/main/java/sample/customer/config/BizConfig.java
@@ -5,17 +5,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
-@ComponentScan("sample.customer.biz.service")
+@ComponentScan("sample.customer.biz.service")   //<----(1)
 public class BizConfig {
 
-    @Bean
+    @Bean                                       //<----(2)
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource
                 = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:/META-INF/messages");
+        messageSource.setBasename("classpath:/META-INF/messages"); //<----(3)
 
         return messageSource;
     }
+
+	// 밑의 4, 5설정은 5.2.3.4절에서 스프링 MVC의 JavaConfig로 이동
+	@Bean                                       //<----(4)
+	public Validator globalValidator(MessageSource messageSource) {
+		LocalValidatorFactoryBean validatorBean 
+				= new LocalValidatorFactoryBean();
+		validatorBean.setValidationMessageSource(messageSource);  //<----(5)
+
+		return validatorBean;
+	}
 }
